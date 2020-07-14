f64fba8946d2fac0e31c539c5eca88eff18b82be
Your Name <you@example.com>
2020-07-14 10:54:26 +0200

    parpadeo enemigo

--- Assets/Scripts/NiaMondoScripts/EnemyHealth.cs
+++ Assets/Scripts/NiaMondoScripts/EnemyHealth.cs
@@ -5,12 +5,43 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;
+    private SpriteRenderer sp;
+
+
+    void Start() {
+        sp = GetComponent<SpriteRenderer>();
+    }
 
     public void ReduceHealth(float n)
     {
         health -= n;
         if (health <= 0f)
             Die();
+        //sp.enabled = false;
+        StartCoroutine(numer());
+        //InvokeRepeating("disable", 0.1f, 5); 
+    }
+
+    IEnumerator numer() { 
+        for(var n = 0; n < 5; n++)
+	    {
+            sp.enabled = false;
+	        yield return new WaitForSeconds(0.1f);
+            sp.enabled = true;
+	        yield return new WaitForSeconds(0.1f);
+	
+	
+	    }
+    }
+
+    void disable() {
+        sp.enabled = false;
+        Invoke("count",0.1f);
+    }
+
+    void count() {
+        //yield return new WaitForSeconds(0.2f);
+        sp.enabled = true; 
     }
 
     public void Die()
